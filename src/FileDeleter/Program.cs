8ca2e3d2c6893f134adf2619468a38386be10d67
Rafael Dutra <rafaeldut89@gmail.com>
2018-12-21 17:42:14 -0200

    Initial commit

---
+++ src/FileDeleter/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileDeleter
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string txtFilePath = Path.Combine(Directory.GetCurrentDirectory(), "arquivos_para_excluir.txt");
+            Console.WriteLine("Caminho completo da pasta 'INFRA V1.0.0':");
+
+            string applicationPath = Console.ReadLine();
+            var lines = File.ReadLines(txtFilePath);
+            foreach (string line in lines)
+            {
+                string fileToExcludePath = applicationPath + line;
+                if (File.Exists(fileToExcludePath))
+                {
+                    File.Delete(applicationPath + line);
+                }
+            }
+        }
+    }
+}
